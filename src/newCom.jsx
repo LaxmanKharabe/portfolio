6585f9b1fef959f5421e14f5c101a3e3128b6b94
LaxmanKharabe <laxman31097@gmail.com>
2024-07-01 20:08:45 +0530

    New component added

---
+++ src/newCom.jsx
@@ -0,0 +1,12 @@
+// src/components/NewComponent.js
+import React from 'react';
+
+const NewComponent = () => {
+  return (
+    <div>
+      <h1>Hello, this is the new component!</h1>
+    </div>
+  );
+};
+
+export default NewComponent;
